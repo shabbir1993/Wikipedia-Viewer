be1dfc038d34d48cc15442e9cd285df135e4a39d
shabbir1993 <syed_shabbir_haider@hotmail.com>
2017-06-22 02:38:02 +0500

    Native fetch call

--- wikiv1/src/App.js
+++ wikiv1/src/App.js
@@ -1,49 +1,69 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function getData(){
-  fetch(`https://api.github.com/users/shabbir1993`)
-    .then(response => response.json())
-    .then(function(result){
-      alert(result);
-    });
-}
-
-class App extends Component {
+const URL_BASE = 'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srsearch=';
+const URL_CALLBACK = '&callback=?JSON_CALLBACK';
+const DEFAULT_KEYWORD = 'hello world'
+class App extends React.Component {
   constructor(props){
     super(props);
-
+    
     this.state = {
-      result: null, 
-      searchTerm: ''
+      keyword: DEFAULT_KEYWORD, 
+      result: ''
     }
-    this.fetchData = this.fetchData.bind(this);
-    this.setResult = this.setResult.bind(this);
+    
+    this.onSearchChange = this.onSearchChange.bind(this);
+    //this.handleSubmit = this.handleSubmit.bind(this);
+    this.onResponse = this.onResponse.bind(this);
+    this.fetchResults = this.fetchResults.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
   }
   
-
-  fetchData(){
-    fetch(`https://api.github.com/users/shabbir1993`)
-    .then(response => response.json())
-    .then(function(result){
-      alert(result);
-    });
-    
+  onSearchChange(event){
+    this.setState({keyword: event.target.value});
+  }
+  
+  onResponse(response){
+    this.setState({result:response});
+    alert(response);
+  }
+  
+  fetchResults(keyword){
+    fetch(`${URL_BASE}${keyword}${URL_CALLBACK}`,{
+      headers: { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+      })
+    .then(response => {this.onResponse(response)});
+  }
+  onSearchSubmit(event){
+    const {keyword} = this.state;
+    this.fetchResults(keyword);
+    event.preventDefault();
   }
 
-  setResult(result){
-    alert(result);
-    console.log(result);
+  componentDidMount(){
+    const {keyword} = this.state;
+    this.fetchResults(keyword);
   }
-
-  render() {
+  
+  render(){
+    const {keyword, result} = this.state;
+    if(!result) {return null;}
     return (
       <div>
-        <input type = "submit" value = "Display" onClick = {getData()} />
+        <Title />
+        <form onSubmit = {this.onSearchSubmit}>
+          <input type = "text" value = {keyword} onChange = {this.onSearchChange} />
+          <input type = "submit" value = "Search" /> 
+        </form> 
+        
       </div>
-      );
+
+    );
   }
 }
-
+const Title = () => <h1>Wikipedia Explorer</h1> 
 export default App;
